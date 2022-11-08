71eca9f945d6e025bb0a986e36b2fa2a2dc4e20f
VICTOR ROZA SOUZA <enricofermi2019@gmail.com>
2022-11-08 16:13:00 -0400

    adicionando login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,4 +32,28 @@ class AuthController extends Controller
 
         return response($response, 201);
     }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email'=>'required|string',
+            'password'=>'required|string'
+        ]);
+
+        $user = User::where('email', $fields['email'])->first();
+
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'E-mail ou senha inválidos'
+            ], 401);
+        }
+
+        $token = $user->createToken('UsuarioLogado')->plainTextToken;
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
 }
